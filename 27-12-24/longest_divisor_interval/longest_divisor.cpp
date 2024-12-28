b8d9356695792d50ed21b7bd2ea3a20bbe6dd567
PushkarShinde <pushkarshinde249@gmail.com>
2024-12-28 15:48:05 +0530

    Potd 27th

--- 27-12-24/longest_divisor_interval/longest_divisor.cpp
+++ 27-12-24/longest_divisor_interval/longest_divisor.cpp
@@ -1,19 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int divisor(long long num){
-
-}
-
 int main() {
     int t;
     cin >> t;
-    vector<long long>nums;
-    for(int i=0;i<t;i++){
-        cin >> nums[i];
+
+    // Precompute factorials up to 20!
+    vector<long long> factorials(21, 1);
+    for (int i = 1; i <= 20; ++i) {
+        factorials[i] = factorials[i - 1] * i;
     }
-    for(int i: nums){
-        cout << divisor(i) << "\n";
+
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        // Find the largest k such that k! divides n
+        int k = 0;
+        for (int i = 1; i <= 20; ++i) {
+            if (n % factorials[i] == 0) {
+                k = i;
+            } else {
+                break;
+            }
+        }
+
+        cout << k << endl;
     }
+
     return 0;
 }
